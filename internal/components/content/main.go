011ee5d6a38851f4e9e2dfb3853b4f05d2818b54
Jacob Cunningham <jacob.cunningham@live.com>
2024-08-11 17:58:53 +0000

    bucket list cleanup

--- internal/components/content/main.go
+++ internal/components/content/main.go
@@ -16,71 +16,11 @@ import (
 	"github.com/jacobscunn07/duchess/internal/style"
 )
 
-const listHeight = 14
-const defaultWidth = 20
-
-var (
-	titleStyle        = style.BoldPrimary // lipgloss.NewStyle().MarginLeft(2)
-	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(style.Green)
-	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
-	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
-)
-
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
-
-// type item struct {
-// 	title, desc string
-// }
-
-// func (i item) Title() string       { return i.title }
-// func (i item) Description() string { return i.desc }
-// func (i item) FilterValue() string { return i.title }
-
-type item string
-
-func (i item) FilterValue() string { return "" }
-
-type itemDelegate struct{}
-
-func (d itemDelegate) Height() int                             { return 1 }
-func (d itemDelegate) Spacing() int                            { return 0 }
-func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
-func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := listItem.(item)
-	if !ok {
-		return
-	}
-
-	str := fmt.Sprintf("%d. %s", index+1, i)
-
-	fn := itemStyle.Render
-	if index == m.Index() {
-		fn = func(s ...string) string {
-			return selectedItemStyle.Render("> " + strings.Join(s, " "))
-		}
-	}
-
-	fmt.Fprint(w, fn(str))
-}
-
 func New() Model {
-
-	l := list.New([]list.Item{}, itemDelegate{}, defaultWidth, listHeight)
-
-	l.Title = "Buckets"
-	l.SetShowStatusBar(false)
-	l.SetFilteringEnabled(false)
-	l.SetShowHelp(false)
-	l.Styles.Title = titleStyle
-	l.Styles.PaginationStyle = paginationStyle
-	l.Styles.HelpStyle = helpStyle
-
 	return Model{
 		style:   style.Border,
 		buckets: []string{},
-		list:    l,
+		list:    NewList(),
 	}
 }
 
@@ -113,20 +53,16 @@ func (m Model) Update(msg interface{}) (Model, tea.Cmd) {
 	case messages.AvailableWindowSizeMsg:
 		m.updateAvailableWindowSize(msg.Width, msg.Height)
 
-		h, v := docStyle.GetFrameSize()
-		m.list.SetSize(msg.Width-h, msg.Height-v)
+		w, h := m.style.GetFrameSize()
+		m.list.SetSize(msg.Width-w, msg.Height-h)
 
 	case s3.ListBucketsQueryMessage:
 		buckets := []list.Item{}
 		for _, b := range msg.Buckets {
-			// m.buckets = append(m.buckets, b.Name)
-			// m.list = list.New()
 			buckets = append(buckets, item(b.Name))
 		}
 
-		// m.list = list.New(buckets, list.NewDefaultDelegate(), 0, 0)
 		m.list.SetItems(buckets)
-		// m.list.Title = "Buckets"
 	case tea.KeyMsg:
 		switch keypress := msg.String(); keypress {
 		case "enter":
@@ -146,14 +82,6 @@ func (m Model) Update(msg interface{}) (Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	// return m.style.Render(
-	// 	lipgloss.JoinVertical(
-	// 		lipgloss.Left,
-	// 		// m.buckets...,
-	// 		docStyle.Render(m.list.View()),
-	// 	),
-	// )
-
 	if m.choice != "" {
 		return quitTextStyle.Render(fmt.Sprintf("%s? Sounds good to me.", m.choice))
 	}
@@ -177,4 +105,57 @@ func (m *Model) updateAvailableWindowSize(w, h int) (int, int) {
 		Width(m.availableWidth)
 
 	return m.availableWidth, m.availableHeight
+}
+
+var (
+	titleStyle        = style.BoldPrimary
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(style.Green)
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	noItemsStyle      = lipgloss.NewStyle().PaddingLeft(2)
+)
+
+type item string
+
+func (i item) FilterValue() string { return "" }
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
+
+func NewList() list.Model {
+	l := list.New([]list.Item{}, itemDelegate{}, 0, 0)
+
+	l.Title = "Buckets"
+	l.SetStatusBarItemName("bucket", "buckets")
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.SetShowHelp(false)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+	l.Styles.NoItems = noItemsStyle
+
+	return l
 }
