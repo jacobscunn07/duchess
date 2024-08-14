d98e241a07d8f5313f4331acf105b549f5d223b3
Jacob Cunningham <jacob.cunningham@live.com>
2024-08-14 22:13:17 +0000

    wip

--- internal/components/aws/s3/bucket_details.go
+++ internal/components/aws/s3/bucket_details.go
@@ -2,30 +2,75 @@ package s3
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/jacobscunn07/duchess/internal/bubbles"
 	"github.com/jacobscunn07/duchess/internal/charmbracelet/bubbletea/messages/aws/s3"
 	"github.com/jacobscunn07/duchess/internal/components"
+	"github.com/jacobscunn07/duchess/internal/messages"
 	"github.com/jacobscunn07/duchess/internal/style"
 
 	s3_data "github.com/jacobscunn07/duchess/internal/data/s3"
 )
 
-func NewBucketDetailsModel(bucket string) components.Model {
-	return BucketDetailsModel{
+func NewBucketDetailsModel(bucket string, options ...func(*BucketDetailsModel)) components.Model {
+	m := &BucketDetailsModel{
 		bucket:  bucket,
 		objects: []string{},
+		list: bubbles.NewList(
+			bubbles.WithTitle("Objects"),
+			bubbles.WithStatusBarItemName("object", "objects"),
+		),
+		style: lipgloss.NewStyle(),
+		tabs: func() string {
+			row := lipgloss.JoinHorizontal(
+				lipgloss.Top,
+				activeTab.Render("Objects"),
+				tab.Render("TBD"),
+			)
+			space := tabGap.Render(strings.Repeat(" ", 0))
+			gap := tabGap.Render(strings.Repeat(" ", max(0, lipgloss.Width(row))))
+			row = lipgloss.JoinHorizontal(lipgloss.Bottom, space, row, gap)
+
+			return row
+		}(),
+	}
+
+	for _, o := range options {
+		o(m)
+	}
+
+	w, h := m.style.GetFrameSize()
+	m.list.SetSize(m.availableWidth-w, m.availableHeight-h-lipgloss.Height(m.tabs))
+
+	return *m
+}
+
+func BucketDetailsModelWithHeight(height int) func(m *BucketDetailsModel) {
+	return func(m *BucketDetailsModel) {
+		m.availableHeight = height
+	}
+}
+
+func BucketDetailsModelWithWidth(width int) func(m *BucketDetailsModel) {
+	return func(m *BucketDetailsModel) {
+		m.availableWidth = width
 	}
 }
 
 type BucketDetailsModel struct {
-	bucket  string
-	objects []string
+	bucket          string
+	objects         []string
+	list            list.Model
+	availableWidth  int
+	availableHeight int
+	style           lipgloss.Style
+	tabs            string
 }
 
 func (m BucketDetailsModel) Init() tea.Cmd {
@@ -44,31 +89,52 @@ func (m BucketDetailsModel) Init() tea.Cmd {
 func (m BucketDetailsModel) Update(msg interface{}) (components.Model, tea.Cmd) {
 	var cmds []tea.Cmd
 	switch msg := msg.(type) {
+
+	case messages.AvailableWindowSizeMsg:
+		m.updateAvailableWindowSize(msg.Width, msg.Height)
+
+		w, h := m.style.GetFrameSize()
+		m.list.SetSize(msg.Width-w, msg.Height-h-lipgloss.Height(m.tabs))
+
 	case s3.ListBucketObjectsQueryMessage:
 		m.objects = msg.Objects
+
+		objects := []list.Item{}
+		for _, o := range msg.Objects {
+			objects = append(objects, bubbles.ListDefaultItem(o))
+		}
+
+		m.list.SetItems(objects)
 	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	cmds = append(cmds, cmd)
+
 	return m, tea.Batch(cmds...)
 }
 
 func (m BucketDetailsModel) View() string {
-	row := lipgloss.JoinHorizontal(
-		lipgloss.Top,
-		activeTab.Render("Objects"),
-		tab.Render("Permissions"),
-	)
-	space := tabGap.Render(strings.Repeat(" ", 0))
-	gap := tabGap.Render(strings.Repeat(" ", max(0, lipgloss.Width(row))))
-	row = lipgloss.JoinHorizontal(lipgloss.Bottom, space, row, gap)
-
 	return lipgloss.JoinVertical(
 		lipgloss.Left,
-		style.BoldPrimary.MarginLeft(2).MarginTop(1).MarginBottom(1).Render(fmt.Sprintf("s3 / buckets / %v", m.bucket)),
-		row,
-		lipgloss.NewStyle().MarginLeft(2).Render(m.objects...))
+		m.tabs,
+		m.list.View(),
+	)
 }
 
 func (m BucketDetailsModel) ViewHeight() int {
 	return lipgloss.Height(m.View())
+}
+
+func (m *BucketDetailsModel) updateAvailableWindowSize(w, h int) (int, int) {
+	frameW, frameH := m.style.GetFrameSize()
+
+	m.availableWidth, m.availableHeight = w-frameW, h-frameH
+
+	m.style = m.style.
+		Height(m.availableHeight).
+		Width(m.availableWidth)
+
+	return m.availableWidth, m.availableHeight
 }
 
 var (
--- internal/components/aws/s3/bucket_list.go
+++ internal/components/aws/s3/bucket_list.go
@@ -2,15 +2,13 @@ package s3
 
 import (
 	"context"
-	"fmt"
-	"io"
 	"log"
-	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/jacobscunn07/duchess/internal/bubbles"
 	"github.com/jacobscunn07/duchess/internal/charmbracelet/bubbletea/messages/aws/s3"
 	"github.com/jacobscunn07/duchess/internal/components"
 	"github.com/jacobscunn07/duchess/internal/messages"
@@ -20,7 +18,10 @@ import (
 func NewListBucketModel() components.Model {
 	return ListBucketModel{
 		style: style.Border,
-		list:  NewList(),
+		list: bubbles.NewList(
+			bubbles.WithTitle("Buckets"),
+			bubbles.WithStatusBarItemName("bucket", "buckets"),
+		),
 	}
 }
 
@@ -29,8 +30,6 @@ type ListBucketModel struct {
 	availableWidth  int
 	availableHeight int
 	list            list.Model
-	choice          string
-	quitting        bool
 }
 
 func (m ListBucketModel) Init() tea.Cmd {
@@ -58,21 +57,25 @@ func (m ListBucketModel) Update(msg interface{}) (components.Model, tea.Cmd) {
 	case s3.ListBucketsQueryMessage:
 		buckets := []list.Item{}
 		for _, b := range msg.Buckets {
-			buckets = append(buckets, item(b.Name))
+			buckets = append(buckets, bubbles.ListDefaultItem(b.Name))
 		}
 
 		m.list.SetItems(buckets)
 	case tea.KeyMsg:
 		switch keypress := msg.String(); keypress {
 		case "enter":
-			i, ok := m.list.SelectedItem().(item)
-			if ok {
-				m.choice = string(i)
-				bm := NewBucketDetailsModel(m.choice)
+			if i, ok := m.list.SelectedItem().(bubbles.ListDefaultItem); ok {
+				bm := NewBucketDetailsModel(
+					string(i),
+					BucketDetailsModelWithHeight(m.availableHeight),
+					BucketDetailsModelWithWidth(m.availableWidth),
+				)
+
 				cmd := bm.Init()
 				cmds = append(cmds, cmd)
 				return bm, tea.Batch(cmds...)
 			}
+
 			return m, tea.Batch(cmds...)
 		}
 	}
@@ -85,13 +88,7 @@ func (m ListBucketModel) Update(msg interface{}) (components.Model, tea.Cmd) {
 }
 
 func (m ListBucketModel) View() string {
-	if m.choice != "" {
-		return quitTextStyle.Render(fmt.Sprintf("%s? Sounds good to me.", m.choice))
-	}
-	if m.quitting {
-		return quitTextStyle.Render("Not hungry? That’s cool.")
-	}
-	return "\n" + m.list.View()
+	return m.list.View()
 }
 
 func (m ListBucketModel) ViewHeight() int {
@@ -108,57 +105,4 @@ func (m *ListBucketModel) updateAvailableWindowSize(w, h int) (int, int) {
 		Width(m.availableWidth)
 
 	return m.availableWidth, m.availableHeight
-}
-
-var (
-	titleStyle        = style.BoldPrimary
-	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(style.Green)
-	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
-	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
-	noItemsStyle      = lipgloss.NewStyle().PaddingLeft(2)
-)
-
-type item string
-
-func (i item) FilterValue() string { return "" }
-
-type itemDelegate struct{}
-
-func (d itemDelegate) Height() int                             { return 1 }
-func (d itemDelegate) Spacing() int                            { return 0 }
-func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
-func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := listItem.(item)
-	if !ok {
-		return
-	}
-
-	str := fmt.Sprintf("%d. %s", index+1, i)
-
-	fn := itemStyle.Render
-	if index == m.Index() {
-		fn = func(s ...string) string {
-			return selectedItemStyle.Render("> " + strings.Join(s, " "))
-		}
-	}
-
-	fmt.Fprint(w, fn(str))
-}
-
-func NewList() list.Model {
-	l := list.New([]list.Item{}, itemDelegate{}, 0, 0)
-
-	l.Title = "Buckets"
-	l.SetStatusBarItemName("bucket", "buckets")
-	l.SetShowStatusBar(false)
-	l.SetFilteringEnabled(false)
-	l.SetShowHelp(false)
-	l.Styles.Title = titleStyle
-	l.Styles.PaginationStyle = paginationStyle
-	l.Styles.HelpStyle = helpStyle
-	l.Styles.NoItems = noItemsStyle
-
-	return l
 }
